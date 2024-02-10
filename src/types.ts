d3beb133083c7edc4e1cc1bef7e7771d4188c001
Patrik Mintěl <ja@patrick115.eu>
2024-02-10 12:33:13 +0100

    Finished types

--- src/types.ts
+++ src/types.ts
@@ -1,39 +1,48 @@
-import type { RequestEvent } from '@sveltejs/kit'
-import type { Router } from './router.js'
-import type { Method, Procedure, TypedProcedure } from './server/procedure.js'
+import type { RequestEvent } from '@sveltejs/kit';
+import type { Router } from './router.js';
+import { Method, Params, Procedure, TypedProcedure } from './server/procedure.js';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Any = any
+export type Any = any;
 
-export type Arrayable<T> = T | T[]
+export type Arrayable<T> = T | T[];
 
-export type Awaitable<T> = T | Promise<T>
+export type Awaitable<T> = T | Promise<T>;
 
 export type AsyncReturnType<T extends (...args: Any) => Any> =
-    ReturnType<T> extends Promise<infer U> ? U : ReturnType<T>
+    ReturnType<T> extends Promise<infer U> ? U : ReturnType<T>;
 
-export type CreateContext = (opts: RequestEvent) => Promise<object>
+export type CreateContext = (opts: RequestEvent) => Promise<object>;
 
 export type ErrorApiResponse = {
-    status: false
-    code: number
-    message: string
-}
+    status: false;
+    code: number;
+    message: string;
+};
 
-export type ReplaceArrayItemsWith<T, R> = T extends Array<Any> ? R[] : T
-export type ReplaceArrayWith<T, R> = T extends Array<Any> ? R : T
+export type ReplaceArrayItemsWith<T, R> = T extends Array<Any> ? R[] : T;
+export type ReplaceArrayWith<T, R> = T extends Array<Any> ? R : T;
 
-export type DeepReplaceWith<O, R> = O extends object
+export type ExtractMethod<T> =
+    T extends Procedure<Params<Any, Any, infer M>>
+        ? M
+        : T extends TypedProcedure<Params<Any, Any, infer M>>
+          ? M
+          : never;
+
+export type DistributeMethods<T> = T extends Any ? ExtractMethod<T> : never;
+
+export type MethodsToRoot<O> = O extends object
     ? {
-          [K in keyof O]: O[K] extends Procedure<Any>
-              ? R
-              : O[K] extends TypedProcedure<Any>
-                ? R
+          [K in keyof O]: O[K] extends Procedure<Params<Any, Any, infer M>>
+              ? M
+              : O[K] extends TypedProcedure<Params<Any, Any, infer M>>
+                ? M
                 : O[K] extends Array<Any>
-                  ? ReplaceArrayWith<O[K], R[]>
-                  : DeepReplaceWith<O[K], R>
+                  ? DistributeMethods<O[K][number]>[]
+                  : MethodsToRoot<O[K]>;
       }
-    : O
+    : O;
 
-export type HydrateDataBuilder = { [key: string]: HydrateDataBuilder | Method | Method[] }
-export type HydrateData<R extends Router<Any>> = DeepReplaceWith<R['endpoints'], Method>
+export type HydrateDataBuilder = { [key: string]: HydrateDataBuilder | Method | Method[] };
+export type HydrateData<R extends Router<Any>> = MethodsToRoot<R['endpoints']>;
